0548675b6c4195df559e62194ef0c02e1a109016
eanusiem <54850779+eanusiem@users.noreply.github.com>
2020-04-29 19:00:43 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Scroll Button
+mainbutton = document.querySelector(".scrollbutton");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction(){
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    mainbutton.style.display = "block";
+	} else {
+    mybutton.style.display = "none";
+	}
+}
+function topFunction(){
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
